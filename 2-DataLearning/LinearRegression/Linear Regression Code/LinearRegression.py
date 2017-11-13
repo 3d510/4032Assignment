42205545032e5f1ba16c2461d8b2e9a874a71662
Hoang <hoangvl16384@gmail.com>
2017-11-13 23:53:06 +0800

    Final Linear Regression Code

--- 2-DataLearning/LinearRegression/Linear Regression Code/LinearRegression.py
+++ 2-DataLearning/LinearRegression/Linear Regression Code/LinearRegression.py
@@ -1,60 +1,38 @@
-print(__doc__)
-
-
-# Code source: Jaques Grobler
-# License: BSD 3 clause
-
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVR,LinearSVR
-from sklearn.neural_network import MLPRegressor
-from sklearn import preprocessing
 
-df = pd.read_csv('InputData/movie_metadata_processed.csv', header=0, sep=',')   # columns names if no header
-#df = pd.read_csv('test.csv', header=0, sep=',')   # columns names if no header
-#df = (df-df.mean())/df.std()
+# Read data frame from csv file
+df = pd.read_csv('InputData/movie_metadata_processed.csv', header=0, sep=',')
 
+# Seperate column 'imdb_score' from other columns
 X = (df.drop('imdb_score',axis=1)).as_matrix()
 y = df['imdb_score'].as_matrix()
 
-#X = df[['actor_1_facebook_likes','actor_2_facebook_likes','actor_3_facebook_likes']].as_matrix()
-#y = df['cast_total_facebook_likes'].as_matrix()
-
-
+# Split train and test data to 7:3
 X_train, X_test, y_train, y_test = train_test_split(
-                      X, y, test_size=0.2, shuffle=False
-)
+                      X, y, test_size=0.3, shuffle=True)
 
 # Create linear regression object
-regr = linear_model.LinearRegression()
-#regr = linear_model.LogisticRegression()
-# regr = SVR(C=10)
-#regr = MLPRegressor(max_iter=5000)
+linearRegr = linear_model.LinearRegression()
 # Train the model using the training sets
-regr.fit(X_train, y_train)
-
+linearRegr.fit(X_train, y_train)
 # Make predictions using the testing set
-y_pred = regr.predict(X_test)
+linear_y_pred = linearRegr.predict(X_test)
+# The root mean squared error
+print("Linear Regression RMSE: %.3f"
+      % np.sqrt(mean_squared_error(y_test, linear_y_pred)))
+# The r2 score
+print('Linear Regression r2 score: %.3f' % r2_score(y_test, linear_y_pred))
 
-# The coefficients
-print('Coefficients: \n', regr.coef_)
-
-# The mean squared error
-print("Root Mean squared error: %.2f"
-      % np.sqrt(mean_squared_error(y_test, y_pred)))
-# Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % r2_score(y_test, y_pred))
-print('Mean: %.2f' % np.mean(y))
-# Plot outputs
-# plt.scatter(X_test, y_test,  color='black')
-# plt.plot(X_test, diabetes_y_pred, color='blue', linewidth=3)
-#
-# plt.xticks(())
-# plt.yticks(())
-
-#plt.show()
+# Do the same for Random Forest Regression
+for i in range(5,26,5):
+    ranForestRegr = RandomForestRegressor(n_estimators=i)
+    ranForestRegr.fit(X_train, y_train)
+    ranForest_y_pred = ranForestRegr.predict(X_test)
+    print("Random Forest Regression ("+str(i)+" trees) RMSE: %.3f"
+          % np.sqrt(mean_squared_error(y_test, ranForest_y_pred)))
+    print("Random Forest Regression ("+str(i)+" trees) r2 score: %.3f" % r2_score(y_test, ranForest_y_pred))
